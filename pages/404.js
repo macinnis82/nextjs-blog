5879b276f4ada60223a6aca4a8badf163b95bec9
Ryan MacInnis <Ryan.MacInnis@LibertyMutual.com>
2023-02-07 10:08:01 -0500

    first commit

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import Link from "next/link"
+import Layout from "../components/layout"
+
+const Custom404 = () => {
+  return (
+    <Layout>
+        <h1>404 - Page Not Found</h1>
+    </Layout>
+    
+  )
+}
+
+export default Custom404
